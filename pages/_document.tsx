5134718196c1e1acb8a2d447a8c2a907bf169262
sushij <sushant.wurk@gmail.com>
2022-02-28 22:38:06 +0530

    font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,8 @@ class CustomDocument extends Document {
       <Head>
         <meta name="theme-color" content="#000000" />
         {/* <meta name="build version" content={packageJson.} /> */}
-        <link rel="icon" href="/images/logo.png" />
-        <link rel="preload" href="/fonts/kremlin.ttf" as="font" crossOrigin="anonymous" />
+        {/* <link rel="icon" href="/images/logo.png" /> */}
+        <link rel="preload" href="/public/fonts/kremlin.ttf" as="font" crossOrigin="anonymous" />
       </Head>
       <body>
         <Main />
