a214583b959acdb4eb9d2db9e2b6e0832ee9b21b
Sushij <sushant.wurk@gmail.com>
2023-04-07 22:53:05 +0530

    Refactor: Removed unnecessary react stuff

--- src/components/Threejs.tsx
+++ src/components/Threejs.tsx
@@ -1,45 +1,28 @@
-import { OrbitControls } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
-import React, { useRef, useState } from "react"
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 function Sphere(props: { color: string }) {
-    return (
-        <mesh>
-            <sphereGeometry args={[2, 64, 64]} />
-            <meshStandardMaterial color={props.color} />
-            <perspectiveCamera args={[45, 1, 0.1, 100]} position={[0, 0, 40]} />
-        </mesh>
-    )
+  return (
+    <mesh>
+      <sphereGeometry args={[2, 64, 64]} />
+      <meshStandardMaterial color={props.color} />
+      <perspectiveCamera args={[45, 1, 0.1, 100]} position={[0, 0, 40]} />
+    </mesh>
+  );
 }
 function ThreeRender() {
-    const root = useRef()
-    const [color, setColor] = useState("#FF9CBE")
-    const [mouseDown, setMouseDown] = useState(false)
-    let rgb = []
-
-    function mouseMove(e: any) {
-        if (mouseDown) {
-        }
-    }
-    return (
-        <Canvas
-            ref={root}
-            style={{ height: "512px", width: "512px" }}
-            onMouseDown={() => setMouseDown(true)}
-            onMouseUp={() => setMouseDown(false)}
-            onMouseLeave={() => setMouseDown(false)}
-            onMouseMove={mouseMove}
-        >
-            <pointLight position={[0, 10, 10]} color="0xffffff" />
-            <Sphere color={color} />
-            <OrbitControls
-                enablePan={false}
-                enableZoom={false}
-                autoRotate={true}
-                autoRotateSpeed={2.5}
-            />
-        </Canvas>
-    )
+  return (
+    <Canvas style={{ height: "512px", width: "512px" }}>
+      <pointLight position={[0, 10, 10]} color="0xffffff" />
+      <Sphere color={"#FF9CBE"} />
+      <OrbitControls
+        enablePan={false}
+        enableZoom={false}
+        autoRotate={true}
+        autoRotateSpeed={5}
+      />
+    </Canvas>
+  );
 }
 
-export default ThreeRender
+export default ThreeRender;
