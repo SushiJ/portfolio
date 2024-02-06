c89a176b4278f5b0b5a693ee17664a9a79d108e2
Sushant Sharma <sushant.wurk@gmail.com>
2024-02-06 23:45:42 +0530

    Fix: remove react (IDK WHY I WAS USING IT?)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,59 +1,60 @@
-import { useState } from "react";
-import { CloseMenuIcon, Logo, MenuIcon } from "./Icons";
-const Links = [
-  { href: "/", name: "Home" },
-  { href: "/about", name: "About" },
-  { href: "/resume.pdf", name: "Resume", target: "_blank" },
-];
-function Navbar() {
-  const [isSideBarOpen, setisSideBarOpen] = useState(false);
-  return (
-    <nav className="flex justify-between items-center py-4 bg-nightOwl-transparent mt-2 lg:mt-8 text-nightOwl-text">
-      <a href="/" className="text-nightOwl-pink">
-        <Logo className="w-12 h-12 lg:w-16 lg:h-16" />
-      </a>
-      <ul className="hidden lg:flex py-4 space-x-4">
-        {Links.map((link) => (
-          <li className="" key={link.name}>
-            <a
-              className="border-[.5px] border-nightOwl-text hover:text-nightOwl-pink hover:border-[.5px] hover:border-nightOwl-pink rounded-sm p-2 tex-sm"
-              href={link.href}
-              target={link.target ? link.target : ""}
-            >
-              {link.name}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <button
-        className="lg:hidden"
-        onClick={() => setisSideBarOpen(!isSideBarOpen)}
-      >
-        <span className="hover:text-nightOwl-pink">
-          {isSideBarOpen ? <CloseMenuIcon /> : <MenuIcon />}
-        </span>
-      </button>
-      {isSideBarOpen && (
-        <div
-          tabIndex={isSideBarOpen ? 1 : -1}
-          className="text-xl space-y-2 p-4 absolute right-0 top-0 bottom-0 z-10 w-32 h-32 rounded-sm 
-            mt-20 mr-0 text-left text-nightOwl-pink backdrop-blur border-[1px] border-gray-800"
-        >
-          {Links.map((link) => (
-            <p key={link.name}>
-              <a
-                className="hover:text-nightOwl-pink"
-                href={link.href}
-                target={link.target ? link.target : ""}
-              >
-                {link.name}
-              </a>
-            </p>
-          ))}
-        </div>
-      )}
-    </nav>
-  );
-}
-
-export default Navbar;
+// import { useState } from "react";
+// import { CloseMenuIcon, Logo, MenuIcon } from "./Icons";
+// const Links = [
+//   { href: "/", name: "Home" },
+//   { href: "/about", name: "About" },
+//   { href: "/resume.pdf", name: "Resume", target: "_blank" },
+// ];
+// function Navbar() {
+//   const [isSideBarOpen, setisSideBarOpen] = useState(false);
+//   return (
+//     <nav className="flex justify-between items-center py-4 bg-nightOwl-transparent mt-2 lg:mt-8 text-nightOwl-text">
+//       <a href="/" className="text-nightOwl-pink">
+//         <Logo className="w-12 h-12 lg:w-16 lg:h-16" />
+//       </a>
+//       <ul className="hidden lg:flex py-4 space-x-4">
+//         {Links.map((link) => (
+//           <li className="" key={link.name}>
+//             <a
+//               className="border-[.5px] border-nightOwl-text hover:text-nightOwl-pink hover:border-[.5px] hover:border-nightOwl-pink rounded-sm p-2 tex-sm"
+//               href={link.href}
+//               target={link.target ? link.target : ""}
+//             >
+//               {link.name}
+//             </a>
+//           </li>
+//         ))}
+//       </ul>
+//       <button
+//         type="button"
+//         className="lg:hidden"
+//         onClick={() => setisSideBarOpen(!isSideBarOpen)}
+//       >
+//         <span className="hover:text-nightOwl-pink">
+//           {isSideBarOpen ? <CloseMenuIcon /> : <MenuIcon />}
+//         </span>
+//       </button>
+//       {isSideBarOpen && (
+//         <div
+//           tabIndex={isSideBarOpen ? 1 : -1}
+//           className="text-xl space-y-2 p-4 absolute right-0 top-0 bottom-0 z-10 w-32 h-32 rounded-sm
+//             mt-20 mr-0 text-left text-nightOwl-pink backdrop-blur border-[1px] border-gray-800"
+//         >
+//           {Links.map((link) => (
+//             <p key={link.name}>
+//               <a
+//                 className="hover:text-nightOwl-pink"
+//                 href={link.href}
+//                 target={link.target ? link.target : ""}
+//               >
+//                 {link.name}
+//               </a>
+//             </p>
+//           ))}
+//         </div>
+//       )}
+//     </nav>
+//   );
+// }
+//
+// export default Navbar;
--- src/components/Threejs.tsx
+++ src/components/Threejs.tsx
@@ -1,28 +1,28 @@
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-
-function Sphere(props: { color: string }) {
-  return (
-    <mesh>
-      <sphereGeometry args={[2, 64, 64]} />
-      <meshStandardMaterial color={props.color} />
-      <perspectiveCamera args={[45, 1, 0.1, 100]} position={[0, 0, 40]} />
-    </mesh>
-  );
-}
-function ThreeRender() {
-  return (
-    <Canvas style={{ height: "512px", width: "512px" }}>
-      <pointLight position={[0, 10, 10]} color="0xffffff" />
-      <Sphere color={"#FF9CBE"} />
-      <OrbitControls
-        enablePan={false}
-        enableZoom={false}
-        autoRotate={true}
-        autoRotateSpeed={5}
-      />
-    </Canvas>
-  );
-}
-
-export default ThreeRender;
+// import { OrbitControls } from "@react-three/drei";
+// import { Canvas } from "@react-three/fiber";
+//
+// function Sphere(props: { color: string }) {
+//   return (
+//     <mesh>
+//       <sphereGeometry args={[2, 64, 64]} />
+//       <meshStandardMaterial color={props.color} />
+//       <perspectiveCamera args={[45, 1, 0.1, 100]} position={[0, 0, 40]} />
+//     </mesh>
+//   );
+// }
+// function ThreeRender() {
+//   return (
+//     <Canvas style={{ height: "512px", width: "512px" }}>
+//       <pointLight position={[0, 10, 10]} color="0xffffff" />
+//       <Sphere color={"#FF9CBE"} />
+//       <OrbitControls
+//         enablePan={false}
+//         enableZoom={false}
+//         autoRotate={true}
+//         autoRotateSpeed={5}
+//       />
+//     </Canvas>
+//   );
+// }
+//
+// export default ThreeRender;
