f1e371cda4c1f0a1b6e2751f95637831528c92a8
Sushant Sharma <sushant.wurk@gmail.com>
2024-12-25 16:47:25 +0530

    new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,14 @@ export default {
   theme: {
     extend: {
       colors: {
+        base: "#222023",
+        text: "#FAEADD",
+        accent: "#F29CB7",
+        accent_yellow: "#FBCF4F",
+        accent_dark_purple: "#522A6F",
+        accent_light_purple: " #DDAAFF",
         gray: zinc,
-        accent: "#FF9CBE",
+        // accent: "#FF9CBE",
         vesper: {
           bg: '#101010',
           bg1: "#1c1c1c",
@@ -32,3 +38,4 @@ export default {
   },
   plugins: [typography()],
 } satisfies Config;
+
