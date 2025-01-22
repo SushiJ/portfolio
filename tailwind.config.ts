0940c7fc45af52a734f158059ceab4ed6018350d
Sushant Sharma <74211783+SushiJ@users.noreply.github.com>
2025-01-22 16:33:06 +0530

    Fix links, add seo, change colors
    * init re-design
    
    * add project section
    
    * Chore. redesign
    
    * Chore. seo
    
    * Fix. links
    
    * new colors
    
    * More color changes
    
    * Chore. change colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,26 +4,14 @@ import { zinc } from "tailwindcss/colors";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html}"],
   theme: {
     extend: {
       colors: {
+        base: "#030303",
+        text: "#EDEDED",
+        accent: "#ffdb89",
         gray: zinc,
-        accent: "#FF9CBE",
-        vesper: {
-          bg: '#101010',
-          bg1: "#1c1c1c",
-          fg: "#fafafa",
-          fg1: '#505050',
-          grey: "#666666",
-
-          cyan: "#99FFE4",
-          red: '#FF8080',
-          yellow: '#FFC799',
-          orange: '#FF7300',
-
-          blue: '#38a89d'
-        }
       },
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
