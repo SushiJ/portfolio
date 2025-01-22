3246f61872b68bf9988b304dbc34efb55e2d6570
SushiJ <sushant.wurk@gmail.com>
2025-01-22 16:21:25 +0530

    Chore. change colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,32 +4,14 @@ import { zinc } from "tailwindcss/colors";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html}"],
   theme: {
     extend: {
       colors: {
-        base: "#222023",
-        text: "#FAEADD",
-        accent: "#F29CB7",
-        accent_yellow: "#FBCF4F",
-        accent_dark_purple: "#522A6F",
-        accent_light_purple: " #DDAAFF",
+        base: "#030303",
+        text: "#EDEDED",
+        accent: "#ffdb89",
         gray: zinc,
-        // accent: "#FF9CBE",
-        vesper: {
-          bg: '#101010',
-          bg1: "#1c1c1c",
-          fg: "#fafafa",
-          fg1: '#505050',
-          grey: "#666666",
-
-          cyan: "#99FFE4",
-          red: '#FF8080',
-          yellow: '#FFC799',
-          orange: '#FF7300',
-
-          blue: '#38a89d'
-        }
       },
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
@@ -38,4 +20,3 @@ export default {
   },
   plugins: [typography()],
 } satisfies Config;
-
